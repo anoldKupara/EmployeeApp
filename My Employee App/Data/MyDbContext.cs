736e45a3413bd84737bb0d41447ad02537b98520
Anold Kupara <kuparaanold20@gmail.com>
2022-11-10 14:54:14 +0200

    added MyDbContext class

---
+++ My Employee App/Data/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace My_Employee_App.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}

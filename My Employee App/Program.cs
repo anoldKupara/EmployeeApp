5263ab10df2434402b146f13526efdd2d332f4d2
Anold Kupara <kuparaanold20@gmail.com>
2022-11-11 10:33:27 +0200

    added the connection strings

--- My Employee App/Program.cs
+++ My Employee App/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using My_Employee_App.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionString")));
 
 var app = builder.Build();
 
